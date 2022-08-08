ac3ac4c73b284d2ea70d47de05e994802e6794a4
96hsjeong <96.hs.jeong@gmail.com>
2022-08-08 17:16:36 +0900

    [BOJ] G5 2493 탑

--- boj/Main_G5_2493_탑.java
+++ boj/Main_G5_2493_탑.java
@@ -2,10 +2,8 @@ package boj;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main_G5_2493_탑 {
